6d3e09df4638f007986c1ba256f335feadedf6c6
Dan Kalinin <daniil551@hotmail.com>
2019-01-06 03:04:04 +0300

    CoreDataExt

---
+++ CoreDataExt/CoreDataExt.h
@@ -0,0 +1,12 @@
+//
+//  CoreDataExt.h
+//  CoreDataExt
+//
+//  Created by Dan Kalinin on 1/6/19.
+//  Copyright © 2019 Dan Kalinin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double CoreDataExtVersionNumber;
+FOUNDATION_EXPORT const unsigned char CoreDataExtVersionString[];
